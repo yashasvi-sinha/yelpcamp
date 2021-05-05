0bbac7d1873d473dc25486904539dbba7ef51a09
Yashasvi Sinha <yashasvi.sinha@attainu.com>
2021-05-05 23:39:10 +0530

    added  basic mongoose

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const User = require('./models/User')
+
+const app = express()
+
+
+const DB_URL = "mongodb+srv://express-attainu:qAmxNDPVztn2rSUG@cluster0.bg4zd.mongodb.net/yelpcamp-attainu?retryWrites=true&w=majority"
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, async (err) => {
+    if (err) throw err
+
+    console.log('Connected')
+
+    // const instance = new User()
+
+    const newUser = {
+        email: "ASDASD",
+        password: "asdasdasd"
+    }
+
+    const user = new User(newUser)
+
+    const result = await user.save()
+
+    console.log(result)
+
+})
+
+
+
+
+app.listen(5000, () => console.log("Server Started"))
