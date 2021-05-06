99a7870e33cc37668c7c81031bdb774c5a0e2f43
Sadaf <jamalsadaf3@gmail.com>
2021-05-07 02:12:31 +0530

    done

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const User = require('./models/User')
+const express = require('express');
+const mongoose = require('mongoose');
+const expHbs = require('express-handlebars');
+const User = require('./models/main');
 
-const app = express()
+const app = express();
+app.use(express.static('./public'));
 
+app.engine('hbs', expHbs({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
 
-const DB_URL = "mongodb+srv://express-attainu:qAmxNDPVztn2rSUG@cluster0.bg4zd.mongodb.net/yelpcamp-attainu?retryWrites=true&w=majority"
+const DB_URL =
+  'mongodb+srv://express-attainu:qAmxNDPVztn2rSUG@cluster0.bg4zd.mongodb.net/yelpcamp-attainu?retryWrites=true&w=majority';
 
-mongoose.connect(DB_URL, {
+mongoose.connect(
+  DB_URL,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
-}, async (err) => {
-    if (err) throw err
+    useCreateIndex: true,
+  },
+  async (err) => {
+    if (err) throw err;
 
-    console.log('Connected')
-
-    // const instance = new User()
+    console.log('Connected');
 
     const newUser = {
-        email: "ASDASD",
-        password: "asdasdasd"
-    }
+      email: 'ASDASD',
+      password: 'asdasdasd',
+    };
 
-    const user = new User(newUser)
+    const user = new User(newUser);
 
-    const result = await user.save()
+    const result = await user.save();
 
-    console.log(result)
+    console.log(result);
+  }
+);
 
-})
+app.get('/', (req, res) => {
+  res.render('landing');
+});
 
+app.get('/campgrounds', (req, res) => {
+  res.render('home');
+});
 
-
-
-app.listen(5000, () => console.log("Server Started"))
+app.listen(3000, () => console.log('Server Started'));
