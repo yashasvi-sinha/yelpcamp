ead12e9acd8b8c691691d916ec17376375eb5f37
Raj-pati-au16 <raj.pati.92222@gmail.com>
2021-05-07 01:38:01 +0530

    adding Landing Route to the App

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const User = require('./models/User')
+const handle=require("express-handlebars")
+const { static } = require('express')
+
+
 
 const app = express()
+app.use(express.static('public'))
+app.engine('hbs', handle({extname:'hbs'}));
+app.set('view engine', 'hbs');
 
+// THis is done because of its showing copied DB_URL
 
-const DB_URL = "mongodb+srv://express-attainu:qAmxNDPVztn2rSUG@cluster0.bg4zd.mongodb.net/yelpcamp-attainu?retryWrites=true&w=majority"
+// const DB_URL = "mongodb+srv://express-attainu:qAmxNDPVztn2rSUG@cluster0.bg4zd.mongodb.net/yelpcamp-attainu?retryWrites=true&w=majority"
 
-mongoose.connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-}, async (err) => {
-    if (err) throw err
+// mongoose.connect(DB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+// }, async (err) => {
+//     if (err) throw err
 
-    console.log('Connected')
+//     console.log('Connected')
 
-    // const instance = new User()
+//     // const instance = new User()
 
-    const newUser = {
-        email: "ASDASD",
-        password: "asdasdasd"
-    }
+//     const newUser = {
+//         email: "ASDASD",
+//         password: "asdasdasd"
+//     }
 
-    const user = new User(newUser)
+//     const user = new User(newUser)
 
-    const result = await user.save()
+//     const result = await user.save()
 
-    console.log(result)
+//     console.log(result)
 
+// })
+
+app.get("/home",(req,res)=>{
+    res.render('landing-page.hbs')
 })
 
+app.get("/routing",(req,res)=>{
+    res.send('routed ')
+})
 
 
 
