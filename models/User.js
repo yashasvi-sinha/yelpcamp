c5a1af62dff8c47401677c67ffdf1fa27e93ff28
Gaurav Singh <gauravsingh.info@gmail.com>
2021-05-08 02:11:48 +0530

    Adding Campground page With Add new form

--- models/User.js
+++ models/User.js
@@ -15,6 +15,39 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
+const UserSchemaNewCampgrounds = new mongoose.Schema({
+    campName:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    imageUrl:{
+        type:String,
+        required:true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
 const UserModel = mongoose.model('User', UserSchema)
+const NewUserModel = mongoose.model('newCampgrounds', UserSchemaNewCampgrounds)
 
 module.exports = UserModel
+module.exports = NewUserModel
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const fileUpload = require("express-fileupload")
 const router = express.Router()
 const UserModel = require('../models/User')
+const NewUserModel = require('../models/User')
+
+router.use(fileUpload())
 
 router.get("/home", (req, res) => {
     console.log("Landing page Started")
@@ -9,6 +13,15 @@ router.get("/home", (req, res) => {
 
 router.get('/signUp', (req, res) => {
     res.render('signup')
+})
+
+router.get("/campgrounds", (req, res) => {
+    console.log("Received Campgrounds Page Request")
+    res.render("campgrounds")
+})
+router.get("/campgrounds/new", (req, res) => {
+    console.log("Received Add New Page Request")
+    res.render("new")
 })
 
 
@@ -27,6 +40,36 @@ router.post('/signUp', async (req, res) => {
         res.send(`Internal Error Occurred: ${error._message}`)
     }
 
+})
+
+let global_data = {}
+router.post("/campgrounds", async(req, res) => {
+    console.log("Added New Campgrounds")
+    let myFile = req.files.filename
+    const {campName, price, location, description} = req.body
+    const data = {
+        campName,
+        price,
+        location,
+        "imageUrl": `campImages/${myFile.name}`,
+        description
+    }
+    console.log(data)
+    global_data = data
+    try {
+        const newCampgrounds = new NewUserModel(global_data)
+        const savednewCampgrounds = await newCampgrounds.save()
+        myFile.mv(`./campImages/${myFile.name}`, (err) => {
+            if (err) {
+                res.send({uploaded: false})
+                return
+            }
+            res.render('campgrounds')
+        })
+    } catch (error) {
+        console.log(error)
+        res.send(`Internal Error Occurred: ${error._message}`)
+    }
 })
 
 
