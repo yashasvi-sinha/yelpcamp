9e2307fedad8b9a25b4319e3ddf2cfa1f8071580
ash <ashwinnema06@gmail.com>
2021-05-17 16:00:20 +0530

    maps

---
+++ maps/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const expHbs = require('express-handlebars')
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended:true}))
+app.engine('hbs', expHbs({extname:'hbs'}))
+app.set('view engine', 'hbs')
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + '/a.html')
+})
+
+app.listen(3000)
---
+++ maps/public/js/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([18.954269,  72.811501], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
